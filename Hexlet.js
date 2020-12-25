7ea21ae1699b74c16b4855ed5c6125f1fb9ae307
fedintsevqq <37490668+fedintsevqq@users.noreply.github.com>
2020-12-25 05:17:49 +0300

    Hexlet.js

---
+++ Hexlet.js
@@ -0,0 +1,32 @@
+//1 lesson
+console.log('Hello, World!');
+//2 lesson
+// You know nothing, Jon Snow!
+//3 lesson
+console.log('Robert');
+console.log('Stannis');
+console.log('Renly');
+//4 lesson
+console.log(9780262531962);
+//5 lesson
+console.log('What Is Dead May Never Die');
+//6 lesson
+console.log(81 / 9);
+//7 lesson
+console.log(6 - -81);
+//8 lesson
+console.log(3 ** 5);
+console.log(-8 / -4);
+//9 lesson
+console.log(((8 / 2) + 5) - (-3 / 2));
+//10 lesson
+console.log((70 * (3 + 4)) / (8 + 2));
+//11 lesson
+console.log(0.39 * 0.22);
+//12 lesson
+console.log((Infinity + Infinity) / 10);
+//13 lesson
+console.log(0 / 0);
+console.log(Math.sqrt(-1));
+//14 lesson
+console.log((5 ** 2) - (3 * 7));
